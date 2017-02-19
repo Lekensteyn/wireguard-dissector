632c614cf3bc1ab52aa18d5e20ff2be38c66b1a7
Peter Wu <peter@lekensteyn.nl>
2017-02-19 13:00:43 +0100

    Dissect remaining fields

--- wg.lua
+++ wg.lua
@@ -14,6 +14,12 @@ local F = {
     timestamp   = ProtoField.bytes("wg.timestamp", "Timestamp"),
     mac1        = ProtoField.bytes("wg.mac1", "mac1"),
     mac2        = ProtoField.bytes("wg.mac2", "mac2"),
+    receiver    = ProtoField.uint32("wg.receiver", "Receiver", base.HEX),
+    empty       = ProtoField.bytes("wg.empty", "Empty"),
+    nonce       = ProtoField.bytes("wg.nonce", "Nonce"),
+    cookie      = ProtoField.bytes("wg.cookie", "Cookie"),
+    counter     = ProtoField.uint64("wg.counter", "Counter"),
+    packet      = ProtoField.bytes("wg.packet", "Packet"),
 }
 proto_wg.fields = F
 
@@ -46,19 +52,40 @@ function dissect_initiator(tvb, pinfo, tree)
     tree:add(F.timestamp,   t(16+12))
     tree:add(F.mac1,        t(16))
     tree:add(F.mac2,        t(16))
-    return tvb:len()
+    return t:offset()
 end
 
 function dissect_responder(tvb, pinfo, tree)
-    return tvb:len()
+    local t = next_tvb(tvb)
+    tree:add(F.type,        t(1))
+    tree:add(F.reserved,    t(3))
+    tree:add(F.sender,      t(4))
+    tree:add(F.receiver,    t(4))
+    tree:add(F.ephemeral,   t(32))
+    tree:add(F.empty,       t(16+0)) -- AEAD
+    tree:add(F.mac1,        t(16))
+    tree:add(F.mac2,        t(16))
+    return t:offset()
 end
 
 function dissect_cookie(tvb, pinfo, tree)
-    return tvb:len()
+    local t = next_tvb(tvb)
+    tree:add(F.type,        t(1))
+    tree:add(F.reserved,    t(3))
+    tree:add(F.receiver,    t(4))
+    tree:add(F.nonce,       t(24))
+    tree:add(F.cookie,      t(16+16)) -- AEAD
+    return t:offset()
 end
 
 function dissect_data(tvb, pinfo, tree)
-    return tvb:len()
+    local t = next_tvb(tvb)
+    tree:add(F.type,        t(1))
+    tree:add(F.reserved,    t(3))
+    tree:add(F.counter,     t(8))
+    local packet_length = tvb:len() - t:offset() -- length includes AEAD tag
+    tree:add(F.packet,      t(packet_length))
+    return t:offset()
 end
 
 local types = {
